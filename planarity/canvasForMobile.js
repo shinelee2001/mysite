e5ae0c7096d6a262f3dccbeaf1eabb7ea0d0d796
Dongchan Lee <shinelee2001@gmail.com>
2024-01-30 02:35:46 -0500

    canvas update

--- planarity/canvasForMobile.js
+++ planarity/canvasForMobile.js
@@ -1,12 +1,12 @@
 // 모바일 기기의 너비를 가져오는 함수
 function getMobileScreenWidth() {
-    return Math.max(window.screen.width, window.screen.height);
+    return window.screen.width;
 }
 
 // 모바일 기기에서는 canvas의 너비를 동적으로 설정하는 함수
 function setCanvasWidthForMobile() {
     var canvas = document.getElementById('gameCanvas');
-    var mobileScreenWidth = 300;
+    var mobileScreenWidth = getMobileScreenWidth();
     canvas.width = mobileScreenWidth; // 모바일 기기의 너비로 설정
 }
 
